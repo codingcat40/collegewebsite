7fc9ea007efbc1b6e9b2c16c8d069e9cc1d7d97a
Naman Matoliya <naman@fieldproxy.com>
2025-04-16 21:11:40 +0530

    contact us page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import './styles/Contact.css'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='contact-us'>
+      <form action="" style={{display: 'flex', flexDirection: 'column',
+        width: '225px', justifyContent:'center', textAlign:'center', margin:"auto",
+        gap:'25px', padding: '14px'
+      }}>
+          <label htmlFor="">First Name</label>
+          <input type="text" />
+
+          <label htmlFor="">Second Name</label>
+          <input type="text" />
+
+
+          <label htmlFor="">Email</label>
+          <input type="email" />
+
+
+          <label htmlFor="">Message Us</label>
+          <input type="text" placeholder='Enter Your message here...' style={{height: '126px'}}/>
+
+          <input type="submit" style={{backgroundColor:'black', color:'white'}} />
+      </form>
+    </div>
   )
 }
 
