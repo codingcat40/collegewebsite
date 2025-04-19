20bd3e4eadee00943a8ac2ba8ba42d7c1a286fd9
Naman Matoliya <naman@fieldproxy.com>
2025-04-19 13:20:55 +0530

    Faculty page add on

--- src/components/Faculty.jsx
+++ src/components/Faculty.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from "react";
+import './styles/Faculty.css'
 
 const Faculty = () => {
   return (
-    <div>Faculty</div>
-  )
-}
+    <div className="Fac-container">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio
+        consequuntur provident tempora voluptas asperiores magni. Explicabo quo
+        saepe harum maxime ex atque autem, quos, reiciendis itaque eligendi
+        temporibus cum commodi dolorem aperiam animi in. Aliquam hic quae
+        inventore ex itaque quo rerum placeat accusamus? Non repellat
+        necessitatibus unde eos, repudiandae officiis sed reprehenderit deserunt
+        nulla optio ex earum quasi? Laudantium inventore aut quibusdam quaerat
+        odit sint quia ea odio fuga necessitatibus. Ab, alias pariatur. Iure cum
+        adipisci qui voluptate, exercitationem repudiandae voluptas fugiat
+        voluptatibus nam maxime optio?  <span style={{fontWeight:'bold', fontSize:'large'}}>Imran Gahizdi</span> doloribus praesentium
+        mollitia consequatur porro? Animi aspernatur cumque mollitia rem
+        reiciendis possimus asperiores perferendis a aliquam voluptates culpa
+        soluta quaerat cupiditate consectetur esse nam praesentium hic enim
+        dolor accusamus repellendus aperiam, fugiat minima officia? Aliquid hic
+        cum vitae quod totam pariatur dolore placeat amet officiis iste
+        doloribus harum obcaecati ex libero et nam minus quidem, tenetur
+        dignissimos modi id assumenda voluptatem esse reiciendis! Quidem libero
+        quasi explicabo id quia velit eaque tempora veniam quisquam fugiat error
+        labore maxime nisi nihil, impedit earum. Recusandae architecto minus
+        repudiandae aut? Ea obcaecati nesciunt beatae omnis tempora eum
+        incidunt, deserunt ab in ratione iste doloribus accusamus eveniet, iure
+        dolore itaque, harum sint explicabo accusantium debitis molestias. Aut,
+        nesciunt inventore iusto et vel ad voluptatum repudiandae atque
+        perferendis ex dolore id quod fugit placeat officiis dolores molestias.
+        Distinctio alias quae eos est ad neque odit asperiores officia eum
+        aliquam harum, beatae iste quo cumque expedita, nisi reiciendis debitis
+        maxime corrupti, unde eius earum. Laboriosam quis repudiandae eum
+        deleniti iusto, dolores, commodi eligendi eaque dolorem quas cupiditate,
+        totam alias ratione! Eveniet dolores molestias consequuntur totam quo
+        recusandae nam nobis quae assumenda. Nobis, modi. Nulla provident
+        consectetur vel molestias asperiores, tempore culpa libero harum nisi
+        eveniet, sit laudantium enim cumque fuga autem possimus fugiat odit
+        dolore sed ad? Recusandae cumque, temporibus culpa quasi suscipit, ipsa
+        voluptates dolore blanditiis harum officiis iste! Optio earum unde
+        libero hic aliquid assumenda iste ducimus odit ad, perferendis vitae
+        illo non explicabo dolores tempora voluptatem provident tenetur
+        doloribus! Mollitia sit quasi aspernatur animi nisi perspiciatis odit.
+        Ullam consectetur blanditiis praesentium quibusdam minima? Placeat alias
+        praesentium facilis cum facere quaerat ea voluptatem maxime, magni unde.
+        Vero consequatur exercitationem eius voluptate voluptatibus maiores, ab
+        soluta dolorem aspernatur dicta temporibus culpa odio eveniet aliquid
+        doloremque quo nisi? Modi suscipit id odio facilis provident optio ea
+        nostrum! Fugiat culpa dignissimos quod quibusdam sit, error eaque minima
+        dolorum modi praesentium consectetur, quidem incidunt aperiam
+        reiciendis. Iste exercitationem veritatis odio sit consectetur fugiat
+        similique! Dicta eos soluta sed a accusamus minima quae sunt ipsa!
+      </p>
+    </div>
+  );
+};
 
-export default Faculty
+export default Faculty;
